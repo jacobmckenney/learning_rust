d5f48f1f384919a1b99bccb69dcbabe714933c50
Jacob McKenney <91638338+jacobmckenney@users.noreply.github.com>
2022-08-20 20:58:46 -0700

    merge_sort

--- algos/src/main.rs
+++ algos/src/main.rs
@@ -1,4 +1,8 @@
 mod merge_sort;
+
+use crate::merge_sort::merge_sort;
 fn main() {
-    println!("Hello, world!");
+    let mut test = vec![3, 8, 2, 5, 2];
+    let mut new = merge_sort(test);
+    println!("{:?}", new);
 }
--- algos/src/merge_sort.rs
+++ algos/src/merge_sort.rs
@@ -1,6 +1,37 @@
-pub fn merge_sort<T>(data: &Vec<T>) {
-    if data.len() == 1 {
-        // split
-        println!("Split!");
+pub fn merge_sort<T: Copy + Ord>(data: Vec<T>) -> Vec<T> {
+    if data.len() <= 1 {
+        return data;
+    }
+    // split
+    let mut left: Vec<T> = merge_sort(data[0..data.len() / 2].to_vec());
+    let mut right: Vec<T> = merge_sort(data[(data.len() / 2)..data.len()].to_vec());
+    let mut ret: Vec<T> = Vec::<T>::new();
+    // sort
+    while left.len() > 0 || right.len() > 0 {
+        match left.get(0) {
+            Some(l) => match right.get(0) {
+                Some(r) => {
+                    if l < r {
+                        ret.push(left.remove(0));
+                    } else {
+                        ret.push(right.remove(0));
+                    }
+                }
+                None => ret.push(left.remove(0)),
+            },
+            None => ret.push(right.remove(0)),
+        }
+    }
+    return ret;
+}
+
+#[cfg(test)]
+mod merge_sort_tests {
+    use crate::merge_sort::merge_sort;
+
+    #[test]
+    pub fn test() {
+        let mut v = vec![3, 8, 1, 5, 2];
+        assert_eq!(merge_sort(v), vec![1, 2, 3, 5, 8])
     }
 }
